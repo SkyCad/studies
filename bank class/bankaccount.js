e9ab85332d17849b35b387da543db52cb5a86744
SkyCad <dylan.moraisgoncalves@apprenant.ifapme.be>
2024-11-30 15:17:57 +0100

    comments add

--- bank class/bankaccount.js
+++ bank class/bankaccount.js
@@ -1,16 +1,19 @@
+// creation de la classe BankAccount
 class BankAccount {
 
     constructor(initialBalance = 1000) {
         this.INTERESTS = 0.03;
         this.date = new Date();
+        // on crée un objet options pour formater la date
         this.options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
         this.time = this.date.toLocaleTimeString('fr-FR');
         this.solde = initialBalance;
         this.historiqueOperations = [];
     }
-
+ // on crée une méthode depot qui prend en paramètre un montant
     depot(depot) {
         if (depot < 0) {
+            // on vérifie si le montant est positif
             console.log("Impossible de déposer un montant négatif");
             return;
         } else {
@@ -19,12 +22,14 @@ class BankAccount {
             console.log("Solde après dépôt : " + this.solde);
         }
     }
-
+// on crée une méthode retrait qui prend en paramètre un montant
     retrait(retrait) {
         if (retrait < 0) {
+            // on vérifie si le montant est positif
             console.log("Impossible de retirer un montant négatif");
             return;
         } else {
+            // on vérifie si le solde est suffisant
             if (this.solde >= retrait) {
                 this.solde -= retrait;
                 this.historiqueOperations.push("Retrait de " + retrait + "€");
@@ -34,13 +39,13 @@ class BankAccount {
             }
         }
     }
-
+// on crée une méthode addInterests qui ajoute des intérêts annuels de 3%
     addInterests() {
         this.solde += parseFloat((this.solde * this.INTERESTS).toFixed(2));
         this.historiqueOperations.push("Intérêts annuels de 3% ajoutés");
         console.log("Solde après ajout des intérêts : " + this.solde);
     }
-
+// on crée une méthode qui affiche l'historique des opérations
     historiqueOperationsfunction() {
         for (let i = 0; i < this.historiqueOperations.length; i++) {
             console.log("Date : " + this.date.toLocaleDateString('fr-FR', this.options));
